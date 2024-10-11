ac93fe6f60be9db10baae38f0be4c7aed3c73696
phamduc <phamcongducx9@gmail.com>
2024-10-11 22:36:26 +0700

    finish swipper slide in index.html

--- src/js/main.js
+++ src/js/main.js
@@ -44,7 +44,60 @@ function reloadSlider(){
     autoSlider=setInterval(()=>{next.onclick()}, 5000)
 }
 
-var autoSlider=setInterval(()=>{next.onclick()}, 5000)
+// var autoSlider=setInterval(()=>{next.onclick()}, 5000)
+
+
+
+
+//
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+swiperWrapper.addEventListener('mousedown', (e) => {
+    isDown = true;
+    swiperWrapper.classList.add('active');
+    startX = e.pageX - swiperWrapper.offsetLeft;
+    scrollLeft = swiperWrapper.scrollLeft;
+});
+swiperWrapper.addEventListener('mouseleave', () => {
+    isDown = false;
+    swiperWrapper.classList.remove('active');
+});
+swiperWrapper.addEventListener('mouseup', () => {
+    isDown = false;
+    swiperWrapper.classList.remove('active');
+});
+swiperWrapper.addEventListener('mousemove', (e) => {
+    if (!isDown) return; 
+    e.preventDefault();
+    const x = e.pageX - swiperWrapper.offsetLeft;
+    const walk = (x - startX) * 3; 
+    swiperWrapper.scrollLeft = scrollLeft - walk;
+});
+
+const swiper_wraper= document.querySelector('.swiper-wrapper')
+const swiper_btn_prev=document.querySelector('.swiper-btn-prev')
+const swiper_btn_next=document.querySelector('.swiper-btn-next')
+const swiper_slide_width=document.querySelector('.swiper-slide').offsetWidth
+
+let value_scrollLeft=0
+swiper_btn_next.onclick=function(){
+    value_scrollLeft=swiper_wraper.scrollLeft
+    console.log(value_scrollLeft)
+    loadSlide(swiper_slide_width)
+}
+swiper_btn_prev.onclick=function(){
+    value_scrollLeft=swiper_wraper.scrollLeft
+    loadSlide(-swiper_slide_width)
+}
+
+function loadSlide(value_px){
+    swiper_wraper.scrollLeft+=value_px
+}
+
 
 
 
