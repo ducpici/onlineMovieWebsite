cca6a1555e546e79ba82abc28ec6e5fc56ff5c5f
phamduc <phamcongducx9@gmail.com>
2024-10-12 12:12:10 +0700

    finish fix multi swiper-slide

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
+//Auto slider
 const slider=document.querySelector('.main-content-slider')
 const items_slider=document.querySelectorAll('.main-content-slider-item')
 const slider_list=document.querySelector('.main-content-slider-list')
 const dots=document.querySelectorAll('.dot')
-
 const prev=document.querySelector('.btn-prev')
 const next=document.querySelector('.btn-next')
-
 const items_length=items_slider.length-1
-
 let index=0
-
 next.onclick=function(){
     if(index+1 > items_length){
         index=0
@@ -26,14 +23,12 @@ prev.onclick=function(){
     }
     reloadSlider()
 }
-
 for(let i in dots){
     dots[i].onclick=function(){
         index=i
         reloadSlider()
     }
 }
-
 function reloadSlider(){
     var item=items_slider[index].offsetLeft
     slider_list.style.left=-item+'px'
@@ -43,61 +38,66 @@ function reloadSlider(){
     clearInterval(autoSlider)
     autoSlider=setInterval(()=>{next.onclick()}, 5000)
 }
-
 // var autoSlider=setInterval(()=>{next.onclick()}, 5000)
+//End Auto slider
 
 
-
-
-//
-const swiperWrapper = document.querySelector('.swiper-wrapper');
-
+//Multi swiper and click
+const swiperWrappers = document.querySelectorAll('.swiper-wrapper');
 let isDown = false;
 let startX;
 let scrollLeft;
+swiperWrappers.forEach(function(swiperWrapper){
+    swiperWrapper.addEventListener('mousedown', (e) => {
+        isDown = true;
+        swiperWrapper.classList.add('active');
+        startX = e.pageX - swiperWrapper.offsetLeft;
+        scrollLeft = swiperWrapper.scrollLeft;
+    });
+    swiperWrapper.addEventListener('mouseleave', () => {
+        isDown = false;
+        swiperWrapper.classList.remove('active');
+    });
+    swiperWrapper.addEventListener('mouseup', () => {
+        isDown = false;
+        swiperWrapper.classList.remove('active');
+    });
+    swiperWrapper.addEventListener('mousemove', (e) => {
+        if (!isDown) return; 
+        e.preventDefault();
+        const x = e.pageX - swiperWrapper.offsetLeft;
+        const walk = (x - startX) * 3; 
+        swiperWrapper.scrollLeft = scrollLeft - walk;
+    });
+})
 
-swiperWrapper.addEventListener('mousedown', (e) => {
-    isDown = true;
-    swiperWrapper.classList.add('active');
-    startX = e.pageX - swiperWrapper.offsetLeft;
-    scrollLeft = swiperWrapper.scrollLeft;
-});
-swiperWrapper.addEventListener('mouseleave', () => {
-    isDown = false;
-    swiperWrapper.classList.remove('active');
-});
-swiperWrapper.addEventListener('mouseup', () => {
-    isDown = false;
-    swiperWrapper.classList.remove('active');
-});
-swiperWrapper.addEventListener('mousemove', (e) => {
-    if (!isDown) return; 
-    e.preventDefault();
-    const x = e.pageX - swiperWrapper.offsetLeft;
-    const walk = (x - startX) * 3; 
-    swiperWrapper.scrollLeft = scrollLeft - walk;
-});
-
-const swiper_wraper= document.querySelector('.swiper-wrapper')
-const swiper_btn_prev=document.querySelector('.swiper-btn-prev')
-const swiper_btn_next=document.querySelector('.swiper-btn-next')
+const swiper_wrapers= document.querySelectorAll('.swiper-wrapper')
+const swiper_btn_prevs=document.querySelectorAll('.swiper-btn-prev')
+const swiper_btn_nexts=document.querySelectorAll('.swiper-btn-next')
 const swiper_slide_width=document.querySelector('.swiper-slide').offsetWidth
-
 let value_scrollLeft=0
-swiper_btn_next.onclick=function(){
-    value_scrollLeft=swiper_wraper.scrollLeft
-    console.log(value_scrollLeft)
-    loadSlide(swiper_slide_width)
-}
-swiper_btn_prev.onclick=function(){
-    value_scrollLeft=swiper_wraper.scrollLeft
-    loadSlide(-swiper_slide_width)
+
+
+for(let i in swiper_btn_nexts){
+    swiper_btn_nexts[i].onclick=function(){
+        value_scrollLeft=swiper_wrapers[i].scrollLeft
+        loadSlide(i, swiper_slide_width)
+    }
 }
 
-function loadSlide(value_px){
-    swiper_wraper.scrollLeft+=value_px
+
+for(let i in swiper_btn_prevs){
+    swiper_btn_prevs[i].onclick=function(){
+        value_scrollLeft=swiper_wrapers[i].scrollLeft
+        loadSlide(i, -swiper_slide_width)
+    }
 }
 
+
+function loadSlide(swiper_index, value_px){
+    swiper_wrapers[swiper_index].scrollLeft+=value_px
+}
+//End Multi swiper and click
 
 
 
