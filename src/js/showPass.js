32e597f67abfc949fc7e5ed388c8e0c8bbee82e4
phamduc <phamcongducx9@gmail.com>
2024-10-14 22:56:02 +0700

    create form login

--- src/js/showPass.js
+++ src/js/showPass.js
@@ -1,13 +1,26 @@
 var pass=document.querySelector('input[name="password"]')
 var show=document.getElementById('show-pass')
 var hidden=document.getElementById('hidden-pass')
+// show.onclick=function(){
+//     show.style='display:none'
+//     hidden.style='display:block'
+//     pass.type='text'
+// }
+// hidden.onclick=function(){
+//     hidden.style='display:none'
+//     show.style='display:block'
+//     pass.type='password'
+// }
+
 show.onclick=function(){
-    show.style='display:none'
-    hidden.style='display:block'
+    show.classList.add('d-none')
+    hidden.classList.add('d-block')
+    hidden.classList.toggle('d-none')
     pass.type='text'
 }
 hidden.onclick=function(){
-    hidden.style='display:none'
-    show.style='display:block'
+    show.classList.toggle('d-none')
+    show.classList.add('d-block')
+    hidden.classList.toggle('d-none')
     pass.type='password'
 }
